dc15efa77a7bceda689c096903deb9a2ff2ac39b
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-26 02:23:31 -0400

    Refactor

--- game.rb
+++ game.rb
@@ -25,38 +25,31 @@ class Game
         @current_player.lives -= 1
       end
 
-      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
-
-      
-
+      display_score # display results
       check_deadplayer # call method check_deadplayer true or false
-
       switch # call method to switch player turn
-
       @new_game = false # set to false after game started for message puts
       
       if @game_over # if true, stop the loop
         break
       end
-      
     end
-
     winner # call method game_over
-
-  end
-
-  def display_score
-  end
-
-  def check_deadplayer
-    @game_over = (@player1.lives <= 2 || @player2.lives <= 2) ? true : false # update variable to true the game is over once this statement met
-  end
-
-  def winner
-    puts "someone won"
   end
 
   def switch
     @current_player = (@current_player == @player1) ? @player2 : @player1 # switch player if current_player match the first player
+  end
+
+  def display_score
+    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+  end
+
+  def check_deadplayer
+    @game_over = (@player1.lives <= 0 || @player2.lives <= 0) ? true : false # update variable to true the game is over once this statement met
+  end
+
+  def winner
+    puts "someone won"
   end
 end
