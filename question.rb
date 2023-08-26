a4d1588e79d05e06a39da0056615e79b86f3dd1b
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-26 01:16:24 -0400

    Testing math question with puts

--- question.rb
+++ question.rb
@@ -3,6 +3,9 @@ class Question
   attr_reader :problem, :total
   
   def initialize
-   puts val1 = rand(1..20)
+    val1 = rand(1..20)
+    val2 = rand(1..20)
+    puts @problem = "What does #{val1} plus #{val2} equal?"
+    puts @total = val1 + val2
   end
 end
