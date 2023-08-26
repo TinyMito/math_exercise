0540cc85547a1db5ef04934faedcb7cea07ae7df
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-26 01:12:02 -0400

    Setup Question class

--- question.rb
+++ question.rb
@@ -1,2 +1,8 @@
 class Question
+  # Getter for problem question and total value for comparison since this need to be access by Game class
+  attr_reader :problem, :total
+  
+  def initialize
+   puts val1 = rand(1..20)
+  end
 end
