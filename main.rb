28ff24cd752e13c9828a21ea290214a667bfdbb1
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-08-26 01:04:40 -0400

    Add new player instances

--- main.rb
+++ main.rb
@@ -2,4 +2,9 @@ require './game'
 require './player'
 require './question'
 
-game1 = Game.new
+# Create new player instance
+p1 = Player.new("Player 1")
+p2 = Player.new("Player 2")
+
+# Create new game instance
+game1 = Game.new(p1, p2)
